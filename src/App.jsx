9867f750ee4d8aae1c40d117ea42ced4965036f8
Nimra Zulfiqar <nimrazulfiqar002@gmail.com>
2024-03-31 12:06:43 +0500

    added all files

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import './App.css'
+import search from './assets/icons/search.svg'
+import { useStateContext } from './Context'
+import { BackgroundLayout, WeatherCard } from './Components'
+
+function App() {
+
+  const [input, setInput] = useState('')
+  const { weather, thisLocation, values, place, setPlace } = useStateContext()
+  // console.log(weather)
+
+  const submitCity = () => {
+    setPlace(input)
+    setInput('')
+  }
+
+  return (
+    <div className='w-full h-screen text-white px-8'>
+      <nav className='w-full p-3 flex justify-between items-center'>
+        <h1 className='font-bold tracking-wide text-3xl text-grey '>Weather App</h1>
+        <div className='bg-white w-[15rem] overflow-hidden shadow-2xl rounded flex items-center p-2 gap-2'>
+          {/* search bar  */}
+          <img src={search} alt="search" className='w-[1.5rem] h-[1.5rem]' />
+          <input onKeyUp={(e) => {
+            if (e.key === 'Enter') {
+              // enter cities through search bar
+              submitCity()
+            }
+          }} type="text" placeholder='Search city' className='focus:outline-none w-full text-[#212121] text-lg' value={input} onChange={e => setInput(e.target.value)} />
+        </div>
+      </nav>
+      <BackgroundLayout></BackgroundLayout>
+      <main className='w-full flex flex-wrap gap-8 py-4 px-[10%] items-center justify-center'>
+        <WeatherCard
+          place={thisLocation}
+          windspeed={weather.wspd}
+          humidity={weather.humidity}
+          temperature={weather.temp}
+          heatIndex={weather.heatindex}
+          iconString={weather.conditions}
+          conditions={weather.conditions}
+        />
+
+      </main>
+    </div>
+  )
+}
+
+export default App
